1e38478b4b5617fd9fbf53aa47deae0a39168211
Kevin <936957321@qq.com>
2018-01-05 22:58:53 +0800

    excel下载

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -13,7 +13,7 @@ use think\Config;
 use think\Controller;
 use think\exception\HttpException;
 use think\Hook;
-use think\Log;
+use think\Loader;
 use think\Request;
 
 class Common extends Controller{
@@ -122,6 +122,56 @@ class Common extends Controller{
             $this->success($this->result["msg"],$url,$this->result["data"]);
         }else{
             $this->error($this->result["msg"],$url,$this->result["data"]);
+        }
+    }
+
+    /**
+     * 返回结果
+     * @param int $code
+     * @return mixed
+     */
+    protected function _export() {
+
+        Config::set('default_return_type','html');
+        ini_set('max_execution_time', '0');
+
+        unset($this->params,$this->request);
+
+        if($this->result["code"]==1){
+            if(!isset($this->result["title"])||empty($this->result["title"])) $this->result["title"]=date("YmdHis");
+            if(!isset($this->result["list"])) $this->result["list"]=[];
+//            $this->success("开始下载");
+            Loader::import("lib.PHPExcel");
+            Loader::import("lib.PHPExcel.IOFactory");
+
+            $phpexcel = new \PHPExcel();
+            $phpexcel->getProperties()
+                ->setCreator("Maarten Balliauw")
+                ->setLastModifiedBy("Maarten Balliauw")
+                ->setTitle("Office 2007 XLSX Test Document")
+                ->setSubject("Office 2007 XLSX Test Document")
+                ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+                ->setKeywords("office 2007 openxml php")
+                ->setCategory("Test result file");
+            $phpexcel->getActiveSheet()->fromArray($this->result["list"]);
+            $phpexcel->getActiveSheet()->setTitle('Sheet1');
+            $phpexcel->setActiveSheetIndex(0);
+
+            /* 生成到浏览器，提供下载 */
+            ob_end_clean();  //清空缓存
+            header('Content-Type: application/vnd.ms-excel');
+            header("Content-Disposition: attachment;filename=".$this->result["title"].".xls");
+            header('Cache-Control: max-age=0');
+            header('Cache-Control: max-age=1');
+            header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+            header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+            header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+            header ('Pragma: public'); // HTTP/1.0
+            $objwriter = \PHPExcel_IOFactory::createWriter($phpexcel, 'Excel5');
+            $objwriter->save('php://output');
+
+        }else{
+            $this->error($this->result["msg"]);
         }
     }
 
